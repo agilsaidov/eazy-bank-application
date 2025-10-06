917d7ae15243090c4efeabf63e1a3c0a04008df1
Aqil Səidov <aqilasa638@gmail.com>
2025-10-07 03:31:57 +0400

    Implement Cards entity class

---
+++ src/main/java/com/project/eazybank/model/Cards.java
@@ -0,0 +1,41 @@
+package com.project.eazybank.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Entity
+@Setter @Getter
+@Table(name = "cards")
+public class Cards {
+
+    @Id
+    @Column(name = "card_id")
+    private long cordId;
+
+    @Column(name = "customer_id")
+    private long customerId;
+
+    @Column(name = "card_number")
+    private String cardNumber;
+
+    @Column(name = "card_type")
+    private String cardType;
+
+    @Column(name = "total_limit")
+    private int totalLimit;
+
+    @Column(name = "amount_used")
+    private int amountUsed;
+
+    @Column(name = "available_amount")
+    private int availableAmount;
+
+    @Column(name = "create_dt")
+    private Date createDt;
+}
