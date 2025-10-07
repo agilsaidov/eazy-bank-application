31cf03781d143cfec0149d6ac1c5a73cb0c79ada
Aqil Səidov <aqilasa638@gmail.com>
2025-10-07 23:59:25 +0400

    Notices controller is ready

---
+++ src/main/java/com/project/eazybank/controller/NoticesController.java
@@ -0,0 +1,27 @@
+package com.project.eazybank.controller;
+
+import com.project.eazybank.model.Notices;
+import com.project.eazybank.repo.NoticesRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class NoticesController {
+    private final NoticesRepository noticesRepo;
+
+    @GetMapping("/notices")
+    public ResponseEntity<List<Notices>> getAllNotices(){
+       List<Notices> notices = noticesRepo.findAllActiveNotices();
+       if(notices != null){
+           return ResponseEntity.ok()
+                   .body(notices);
+       }
+       return null;
+    }
+}
