0d0f817bc3653c604b8fc84264c9bee1bc7ed7c3
Aqil Səidov <aqilasa638@gmail.com>
2025-10-08 00:03:32 +0400

    cacheControl added to Notices controller

--- src/main/java/com/project/eazybank/controller/NoticesController.java
+++ src/main/java/com/project/eazybank/controller/NoticesController.java
@@ -3,12 +3,14 @@ package com.project.eazybank.controller;
 import com.project.eazybank.model.Notices;
 import com.project.eazybank.repo.NoticesRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.CacheControl;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,6 +22,7 @@ public class NoticesController {
        List<Notices> notices = noticesRepo.findAllActiveNotices();
        if(notices != null){
            return ResponseEntity.ok()
+                   .cacheControl(CacheControl.maxAge(100, TimeUnit.SECONDS))
                    .body(notices);
        }
        return null;
