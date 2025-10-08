bb1951603c8b1f69d35484c0fc26efdb18b8749c
Aqil Səidov <aqilasa638@gmail.com>
2025-10-09 03:39:09 +0400

    Configured endpoints for role-based privileges

--- src/main/java/com/project/eazybank/security/ProjectSecurityConfig.java
+++ src/main/java/com/project/eazybank/security/ProjectSecurityConfig.java
@@ -47,7 +47,11 @@ public class ProjectSecurityConfig {
                 .addFilterAfter(new CsrfCookieFilter(), BasicAuthenticationFilter.class)
                 .requiresChannel(rcc -> rcc.anyRequest().requiresInsecure())
                 .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/myBalance","/myAccount","/myLoans","myCards").authenticated()
+                        .requestMatchers("/myAccount").hasRole("USER")
+                        .requestMatchers("/myBalance").hasAnyRole("USER", "ADMIN")
+                        .requestMatchers("/myLoans").hasRole("USER")
+                        .requestMatchers("/myCards").hasRole("USER")
+                        .requestMatchers("/user").authenticated()
                         .requestMatchers("/notices","/contact","/error","/register","/invalidSession").permitAll());
 
         http.formLogin(withDefaults());
--- src/main/resources/sql/scripts.sql
+++ src/main/resources/sql/scripts.sql
@@ -173,20 +173,6 @@ CREATE TABLE `authorities` (
 );
 
 INSERT INTO `authorities` (`customer_id`, `name`)
-VALUES (1, 'VIEWACCOUNT');
-
-INSERT INTO `authorities` (`customer_id`, `name`)
-VALUES (1, 'VIEWCARDS');
-
-INSERT INTO `authorities` (`customer_id`, `name`)
-VALUES (1, 'VIEWLOANS');
-
-INSERT INTO `authorities` (`customer_id`, `name`)
-VALUES (1, 'VIEWBALANCE');
-
-DELETE FROM `authorities`;
-
-INSERT INTO `authorities` (`customer_id`, `name`)
 VALUES (1, 'ROLE_USER');
 
 INSERT INTO `authorities` (`customer_id`, `name`)
