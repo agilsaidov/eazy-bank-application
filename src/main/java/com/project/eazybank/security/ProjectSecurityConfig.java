4fa8725270193aca493bf72c2ac6b3b4bdf38232
Aqil Səidov <aqilasa638@gmail.com>
2025-10-08 20:50:10 +0400

    Initial version of SecurityFilterChain implemented

--- src/main/java/com/project/eazybank/security/ProjectSecurityConfig.java
+++ src/main/java/com/project/eazybank/security/ProjectSecurityConfig.java
@@ -1,4 +1,52 @@
 package com.project.eazybank.security;
 
+import com.project.eazybank.filter.CsrfCookieFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Collections;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
 public class ProjectSecurityConfig {
+    public SecurityFilterChain httpSecurity(HttpSecurity http) throws Exception {
+        CsrfTokenRequestAttributeHandler csrfTokenRequestAttributeHandler = new CsrfTokenRequestAttributeHandler();
+        http.securityContext(contextConfig -> contextConfig.requireExplicitSave(false))
+                .sessionManagement(sessionConfig -> sessionConfig.sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
+                .cors(corsConfig -> corsConfig.configurationSource(new CorsConfigurationSource() {
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                        CorsConfiguration config = new CorsConfiguration();
+                        config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+                        config.setAllowedMethods(Collections.singletonList("*"));
+                        config.setAllowCredentials(true);
+                        config.setAllowedHeaders(Collections.singletonList("*"));
+                        config.setMaxAge(3600L);
+                        return config;
+                    }
+                }))
+                .csrf(csrfConfig -> csrfConfig.csrfTokenRequestHandler(csrfTokenRequestAttributeHandler)
+                        .ignoringRequestMatchers("/contact","/register")
+                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
+                .addFilterAfter(new CsrfCookieFilter(), BasicAuthenticationFilter.class)
+                .requiresChannel(rcc -> rcc.anyRequest().requiresInsecure())
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/myBalance","/myAccount","/myLoans","myCards").authenticated()
+                        .requestMatchers("/notices","/contact","/error","/register","/invalidSession").permitAll());
+
+        http.formLogin(withDefaults());
+        http.httpBasic(withDefaults());
+        http.exceptionHandling(withDefaults());
+        return http.build();
+    }
+
 }
